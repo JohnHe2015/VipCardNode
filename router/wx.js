29d98702ff9f41917a27108803cadb69c350a4e9
johnHe <xiaozhemu@163.com>
2018-12-26 00:13:19 +0800

    alter wx auth interface

--- router/wx.js
+++ router/wx.js
@@ -96,22 +96,81 @@ router.get('/config/get',(req,res,next)=>{
 })
 
 router.get('/auth',(req,res,next)=>{
-    let scope = "snsapi_userinfo" || "snsapi_base"    //default snsapi_base
-    let state = "";
-    let url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wxConfig.wx.appID}&redirect_uri=${wxConfig.wx.redirect_url}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
-    request(url, (err,response,body)=>{
-        if(err) console.log(err)
-        else
-        {
-            if(response && response.statusCode == "200")
-            {
-                console.log(JSON.stringify(response));
-                res.send(body);
-                res.end();
+    // let scope = "snsapi_userinfo" || "snsapi_base"    //default snsapi_base
+    // let state = "";
+    // let url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wxConfig.wx.appID}&redirect_uri=${wxConfig.wx.redirect_url}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
+    // request(url, (err,response,body)=>{
+    //     if(err) console.log(err)
+    //     else
+    //     {
+    //         if(response && response.statusCode == "200")
+    //         {
+    //             console.log(JSON.stringify(response));
+    //             res.send(body);
+    //             res.end();
+    //         }
+    //     }
+    // })
+    var router = 'wx/get_wx_access_token';
+    // 这是编码后的地址
+    var return_uri = encodeURIComponent('http://96a7f651.ngrok.io/'+router);
+    var scope = 'snsapi_userinfo';
+    res.redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wxConfig.wx.appID}&redirect_uri=${return_uri}&response_type=code&scope=${scope}&state=STATE#wechat_redirect`);
+    //res.redirect('https://open.weixin.qq.com/connect/oauth2/authorize?appid='+AppID+'&redirect_uri='+return_uri+'&response_type=code&scope='+scope+'&state=STATE#wechat_redirect');
+
+})
+
+
+
+router.get('/get_wx_access_token', function(req,res, next){
+    //console.log("get_wx_access_token")
+    //console.log("code_return: "+req.query.code)
+    
+    // 第二步：通过code换取网页授权access_token
+    var code = req.query.code;
+    request.get(
+        {   
+            url:`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${wxConfig.wx.appID}&secret=${wxConfig.wx.appsecret}&code=${code}&grant_type=authorization_code`,
+        },
+        function(error, response, body){
+            if(response.statusCode == 200){
+                
+                // 第三步：拉取用户信息(需scope为 snsapi_userinfo)
+                //console.log(JSON.parse(body));
+                var data = JSON.parse(body);
+                var access_token = data.access_token;
+                var openid = data.openid;
+                
+                request.get(
+                    {
+                        url:'https://api.weixin.qq.com/sns/userinfo?access_token='+access_token+'&openid='+openid+'&lang=zh_CN',
+                    },
+                    function(error, response, body){
+                        if(response.statusCode == 200){
+                            
+                            // 第四步：根据获取的用户信息进行对应操作
+                            var userinfo = JSON.parse(body);
+                            //console.log(JSON.parse(body));
+                            console.log('获取微信信息成功！');
+                            
+                            // 小测试，实际应用中，可以由此创建一个帐户
+                            res.send("\
+                                <h1>"+userinfo.nickname+" 的个人信息</h1>\
+                                <p><img src='"+userinfo.headimgurl+"' /></p>\
+                                <p>"+userinfo.city+"，"+userinfo.province+"，"+userinfo.country+"</p>\
+                            ");
+                            
+                        }else{
+                            console.log(response.statusCode);
+                        }
+                    }
+                );
+            }else{
+                console.log(response.statusCode);
             }
         }
-    })
-})
+    );
+});
 
 
 module.exports = router;
